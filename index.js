46aed14e97ca85382e76df44669fed2af8232902
Jhonny Callisaya <jhonnyeddy.22@gmail.com>
2024-04-19 01:40:27 -0400

    add whitlist to cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const service = new DataService();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: '*', // ['http://localhost:5173/', 'https://form-miro-poblacion.vercel.app/'], // Permite solicitudes desde cualquier origen. Cambia esto por el dominio específico si deseas restringir el acceso.
+  origin: ['http://localhost:5173/', 'https://form-miro-poblacion.vercel.app/'], // Permite solicitudes desde cualquier origen. Cambia esto por el dominio específico si deseas restringir el acceso.
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204
